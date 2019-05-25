5561738603b5221f6df05bd2473ef20c65bd92c8
ilikerobots <mike@hoolehan.com>
2019-05-25 16:00:41 +0300

    two new views for testing

--- django_vue_mpa/settings.py
+++ django_vue_mpa/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'django_vue_mpa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django_vue_mpa/urls.py
+++ django_vue_mpa/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("vue_app_01/", TemplateView.as_view(template_name="vue_app_01.html"), name="vue_app_01"),
+    path("vue_app_02/", TemplateView.as_view(template_name="vue_app_02.html"), name="vue_app_02"),
 ]
